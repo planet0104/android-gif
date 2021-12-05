abf00e374ea764eba90d130a2fccbe70a4f6a581
Jia Ye <planet0104@gmail.com>
2021-12-05 18:03:59 +0800

    java 11

--- gif/src/main/java/run/ccfish/android/gif/Frame.java
+++ gif/src/main/java/run/ccfish/android/gif/Frame.java
@@ -3,37 +3,50 @@ package run.ccfish.android.gif;
 import android.graphics.Bitmap;
 
 public class Frame {
-    /// Frame delay in units of 10 ms.
+    /** Frame delay in units of 10 ms. */
     public final int delay;
     /**
      * Disposal method.
-     *     /// StreamingDecoder is not required to take any action.
+     *     // StreamingDecoder is not required to take any action.
      *     Any = 0,
-     *     /// Do not dispose.
+     *     // Do not dispose.
      *     Keep = 1,
-     *     /// Restore to background color.
+     *     // Restore to background color.
      *     Background = 2,
-     *     /// Restore to previous.
+     *     // Restore to previous.
      *     Previous = 3,
      */
     public final int dispose;
-    /// Transparent index (if available).
+    /** Transparent index (if available). */
     public final int transparent;
-    /// True if the frame needs user input to be displayed.
+    /** True if the frame needs user input to be displayed. */
     public final boolean needs_user_input;
-    /// Offset from the top border of the canvas.
+    /** Offset from the top border of the canvas. */
     public final int top;
-    /// Offset from the left border of the canvas.
+    /** Offset from the left border of the canvas. */
     public final int left;
-    /// Width of the frame.
+    /** Width of the frame. */
     public final int width;
-    /// Height of the frame.
+    /** Height of the frame. */
     public final int height;
-    /// True if the image is interlaced.
+    /** True if the image is interlaced. */
     public final boolean interlaced;
-    /// image data
+    /** image data */
     public final Bitmap image;
 
+    /**
+     * Frame
+     * @param delay
+     * @param dispose
+     * @param transparent
+     * @param needs_user_input
+     * @param top
+     * @param left
+     * @param width
+     * @param height
+     * @param interlaced
+     * @param image
+     */
     public Frame(int delay, int dispose, int transparent, boolean needs_user_input, int top, int left, int width, int height, boolean interlaced, Bitmap image) {
         this.delay = delay;
         this.dispose = dispose;
@@ -45,5 +58,9 @@ public class Frame {
         this.height = height;
         this.interlaced = interlaced;
         this.image = image;
+    }
+
+    public int getDelayMs() {
+        return delay * 10;
     }
 }
